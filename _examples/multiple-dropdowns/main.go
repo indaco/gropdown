bd7ff3906cb16c54c51062f184814f4de3a0b7a7
indaco <github@mircoveltri.me>
2024-05-04 14:12:31 +0200

    chore: add multiple dropdown example

---
+++ _examples/multiple-dropdowns/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/indaco/gropdown"
+)
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	configOne := gropdown.NewConfigBuilder().WithPosition(gropdown.Top).Build()
+	configTwo := gropdown.NewConfigBuilder().WithCloseOnOutsideClick(false).Build()
+	configMap := gropdown.NewConfigMap()
+	configMap.Add("menu-1", configOne)
+	configMap.Add("menu-2", configTwo)
+
+	ctx := context.WithValue(r.Context(), gropdown.ConfigContextKey, configMap)
+	err := HomePage().Render(ctx, w)
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", HandleHome)
+
+	port := ":3300"
+	log.Printf("Listening on %s", port)
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Printf("failed to start server: %v", err)
+		os.Exit(1)
+	}
+}
